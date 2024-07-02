f0053b33e015534010641b54ee421d3ea1a9a528
decl5th <alsgur6624@gmail.com>
2024-07-02 15:50:43 +0900

    웹 게임 연결, 뽑기 기능 테스트를 위한 mypage 도메인 수정

--- src/main/webapp/static/js/pick/info.js
+++
@@ -1,7 +0,0 @@
-function callbackProfile(data) {
-	const { popup } = commonLib;
-	const rootUrl = document.querySelector("meta[name='rootUrl']").content;
-
-	const url = `${rootUrl}pokemon/popup/${data.seq}`;
-	popup.open(url, 500, 500);
-}
