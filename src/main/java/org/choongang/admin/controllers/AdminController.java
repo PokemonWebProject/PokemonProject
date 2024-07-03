8529c1ed3e5bd4110f402640fd7c34dd2984ca4b
sswum <l5450won@naver.com>
2024-07-03 12:02:13 +0900

    관리자 게시판 연결중

--- src/main/java/org/choongang/admin/controllers/AdminController.java
+++ src/main/java/org/choongang/admin/controllers/AdminController.java
@@ -1,5 +1,7 @@
 package org.choongang.admin.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Controller;
 import org.choongang.global.config.annotations.GetMapping;
 import org.choongang.global.config.annotations.RequestMapping;
@@ -8,13 +10,25 @@ import org.choongang.global.config.annotations.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
-
-
     @GetMapping
     public String index() {
         return "admin/index";
-         }
-
     }
+
+
+
+
+    @GetMapping("/board")
+    //관리자쪽 게시판 목록
+    public String board(HttpServletRequest request) {
+
+
+
+
+        return "admin/board";
+    }
+
+
+}
 
 
